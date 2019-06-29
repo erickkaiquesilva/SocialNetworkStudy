83fe63c369f952220bb7a2173d822530e0c709d5
erickkaiquesilva <erickkay272@gmail.com>
2019-06-29 20:10:03 -0300

    Connect DataBase

--- server-nodeJS/node-express/teste/db/connect.js
+++ server-nodeJS/node-express/teste/db/connect.js
@@ -1,16 +1,44 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test'
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
 });
 
-connection.connect();
+connection.connect(function (err) {
+    if (err) return console.log(err);
+    console.log('conectou!');
+    createTable(connection);
+    addRows(connection)
+})
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
 
-connection.end();
+function createTable(connection) {
+
+    const sql = "CREATE TABLE IF NOT EXISTS Clientes (\n" +
+        "ID int NOT NULL AUTO_INCREMENT,\n" +
+        "Nome varchar(150) NOT NULL,\n" +
+        "CPF char(11) NOT NULL,\n" +
+        "PRIMARY KEY (ID)\n" +
+        ");";
+
+    connection.query(sql, function (error, results, fields) {
+        if (error) return console.log(error);
+        console.log('criou a tabela!');
+    });
+}
+
+function addRows(connection){
+  const sql = "INSERT INTO Clientes(Nome,CPF) VALUES ?";
+  const values = [
+        ['teste1', '12345678901'],
+        ['teste1', '09876543210'],
+        ['teste3', '12312312399']
+      ];
+    connection.query(sql, [values], function (error, results, fields){
+          if(error) return console.log(error);
+          console.log('adicionou registros!');
+          conn.end();//fecha a conexão
+      });
+}
